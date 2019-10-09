ae702f5573a3a267544d34565b1102d332c622d0
Tilman M. Jaeschke <asciibeats@users.noreply.github.com>
2019-10-09 16:56:35 +0200

    implemented upstream changes

--- src/lterralib.cc
+++ src/lterralib.cc
@@ -7,24 +7,26 @@ using namespace std;
 
 static int terra_gensquare(lua_State *L)
 {
-  int seed = luaL_checkinteger(L, 1);
-  int size = luaL_checkinteger(L, 2);
-  float frequency = luaL_checknumber(L, 3);
-  int octaves = luaL_checkinteger(L, 4);
-  float lacunarity = luaL_checknumber(L, 5);
-  float persistence = luaL_checknumber(L, 6);
-  float erosion = luaL_checknumber(L, 7);
-  float water = luaL_checknumber(L, 8);
-  float heat = luaL_checknumber(L, 9);
+  int seed = luaL_optinteger(L, 1, 7);
+  int size = luaL_optinteger(L, 2, 32);
+  int depth = luaL_optinteger(L, 3, 4);
+  float frequency = luaL_optnumber(L, 4, 1.0f);
+  int octaves = luaL_optinteger(L, 5, 2);
+  float lacunarity = luaL_optnumber(L, 6, 0.5f);
+  float persistence = luaL_optnumber(L, 7, 2.0f);
+  float erosion = luaL_optnumber(L, 8, 0.5f);
 
   SquareMap smap;
-  smap.generate(seed, size, frequency, octaves, lacunarity, persistence, erosion, water, heat);
+  smap.generate(seed, size, depth, frequency, octaves, lacunarity, persistence, erosion);
   lua_createtable(L, 0, 3);
-  lua_pushliteral(L, "size");
-  lua_pushinteger(L, smap.size);
+  lua_pushliteral(L, "seed");
+  lua_pushinteger(L, seed);
   lua_rawset(L, -3);
-  lua_pushliteral(L, "width");
-  lua_pushinteger(L, smap.width);
+  lua_pushliteral(L, "size");
+  lua_pushinteger(L, size);
+  lua_rawset(L, -3);
+  lua_pushliteral(L, "depth");
+  lua_pushinteger(L, depth);
   lua_rawset(L, -3);
   lua_pushliteral(L, "tiles");
   lua_createtable(L, 0, smap.tiles.size());
